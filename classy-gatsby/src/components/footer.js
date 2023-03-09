e015b6078b42926b5e3342738691fcadd29dfd72
chakra-digital <info@chakra.digital>
2023-03-09 12:45:57 -0600

    updated footer info

--- classy-gatsby/src/components/footer.js
+++ classy-gatsby/src/components/footer.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { buildImageObj } from "../lib/helpers";
-import { imageUrlFor } from "../lib/image-url";
+import React from 'react';
+import {useStaticQuery, graphql} from 'gatsby';
+import {buildImageObj} from '../lib/helpers';
+import {imageUrlFor} from '../lib/image-url';
 
 const query = graphql`
   fragment SanityImage on SanityMainImage {
@@ -58,27 +58,25 @@ const Footer = () => {
 
   return (
     <footer>
-      <div className="container">
-        <div className="sm:w-[450px] mx-auto w-fill">
-          <div className="flex flex-row justify-between py-[60px]">
-            {socialIcon.map((item) => (
+      <div className='container'>
+        <div className='sm:w-[450px] mx-auto w-fill'>
+          <div className='flex flex-row justify-between py-[60px]'>
+            {socialIcon.map(item => (
               <div
-                className="border-2 border-white rounded-[44px] w-[110px] py-[13px]"
-                key={item.node.name}
-              >
+                className='border-2 border-white rounded-[44px] w-[110px] py-[13px]'
+                key={item.node.name}>
                 <a
                   href={item.node.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-black h-7 w-7"
-                >
+                  target='_blank'
+                  rel='noreferrer'
+                  className='bg-black h-7 w-7'>
                   {item.node.icon && item.node.icon.asset && (
                     <img
                       src={imageUrlFor(buildImageObj(item.node.icon))
-                        .fit("crop")
-                        .auto("format")
+                        .fit('crop')
+                        .auto('format')
                         .url()}
-                      className="m-auto"
+                      className='m-auto'
                       alt={item.node.icon.alt}
                     />
                   )}
@@ -90,16 +88,18 @@ const Footer = () => {
             {logoimage && logoimage.asset && (
               <img
                 src={imageUrlFor(buildImageObj(logoimage))
-                  .fit("crop")
-                  .auto("format")
+                  .fit('crop')
+                  .auto('format')
                   .url()}
-                className="m-auto w-full"
+                className='m-auto w-full'
                 alt={logoimage.alt}
               />
             )}
           </div>
-          <div className="text-center pt-[24px] pb-[60px]">
-            <p className="font-normal text-[16px] leading-[21px] text-white">© 2022, Classy Jackass. All Rights Reserved.</p>
+          <div className='text-center pt-[24px] pb-[60px]'>
+            <p className='font-normal text-[16px] leading-[21px] text-white'>
+              © 2023, Classy World LLC. All Rights Reserved.
+            </p>
           </div>
         </div>
       </div>
